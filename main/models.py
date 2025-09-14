2c624062135930787c85a2e9f551e6f39aeff3e2
Alvino0706 <alvino0706@gmail.com>
2025-09-14 16:41:02 +0700

    Tugas 2 error badly formed hexadecimal UUID String

--- main/models.py
+++ main/models.py
@@ -1,10 +1,31 @@
+import uuid
 from django.db import models
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('jersey', 'Jersey'),
+        ('update', 'Update'),
+        ('sepatu', 'Sepatu'),
+        ('topi', 'Topi'),
+        ('celana', 'Celana'),
+        ('botol minum', 'Botol Minum'),
+    ]
+
+    CONDITION_CHOICES = [
+        ('baru', 'Baru'),
+        ('bekas', 'Bekas'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    price = models.IntegerField(default=0)
+    price = models.PositiveIntegerField(default=0)
     description = models.TextField()
+    stock =  models.PositiveIntegerField(default=0)
+    condition = models.CharField(max_length=20, choices=CONDITION_CHOICES, default='baru')
     thumbnail = models.URLField(blank=True, null=True)
-    category = models.CharField(default='update')
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='jersey')
     is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
     
